0162aa99d4aeebbe46bac23b598ca2f9025a5e8d
Furkan ERAVSAR <eravsarfurkan@gmail.com>
2023-03-07 14:47:43 +0300

    uygulama bitti

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyBHTIP2dof4GXU04JakIRYo-Szr8RlReLk")
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
